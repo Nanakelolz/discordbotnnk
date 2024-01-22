4f13a85c3d99809d03dbf721212635221a6ffd9c
Nanakelol <contact@le-pixel.com>
2024-01-22 14:28:40 +0100

    Retrait de la partie histoire du bot

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 import { REST, Routes, Client, GatewayIntentBits } from 'discord.js';
-import keys from './varprod.json' assert {type: 'json'};
 console.log(keys,keys["APIKEY"])
 const rest = new REST({ version: '10' }).setToken(keys["APIKEY"]);
 import fetch from 'node-fetch'; // Importez le module fetch
-
-import histoires  from './histoirennk.json' assert {type: 'json'};
 const fs = import('fs');
-const JSON_FILE = 'histoirennk.json';
 const userIDFeminin = ['346737432869470219','349898990088749056','280804408457953280','776137866636034079','152527402252238848','280804408457953280','270279322755923969'];
 const client = new Client({
 	intents: [ // Toutes les autorisations du BOT
@@ -18,28 +14,6 @@ const client = new Client({
     GatewayIntentBits.GuildMessageReactions,
 	],
 });
-const commands = [
-  {
-    name: 'histoire',
-    description: 'Obtenir une histoire aléatoire',
-  },
-];
-/*const commands = [
-  {
-    name: 'ping',
-    description: 'Replies with Pong!',
-  },
-  {
-    name: 'papillon',
-    description: 'Replies with de lumière!',
-  },
-];
-
-
-function getLastWord(str) {
-  return (str.toLowerCase().match(/(\w+)\W*$/) || [])[1];
-} */
-
 try {
   console.log('Started refreshing application (/) commands.');
 
@@ -49,34 +23,6 @@ try {
 } catch (error) {
   console.error(error);
 }
-
-client.on('interactionCreate', async (interaction) => {
-  if (!interaction.isCommand()) return;
-
-  if (interaction.commandName === 'histoire') {
-    try {
-      // Charger le contenu du fichier JSON en utilisant require
-      const data = histoires;
-
-      const response = [];
-
-      for (const bloc in data) {
-        if (data.hasOwnProperty(bloc)) {
-          const phrases = data[bloc];
-          const phraseAleatoire = phrases[Math.floor(Math.random() * phrases.length)];
-          response.push(`${phraseAleatoire}`);
-        }
-      }
-
-      // Envoyer les phrases choisies dans la réponse de la commande
-      await interaction.reply(response.join(' '));
-    } catch (error) {
-      console.error(error);
-      await interaction.reply('Une erreur s\'est produite lors de la récupération de l\'histoire.');
-    }
-  }
-});
-
 client.on('messageCreate', (message) => {
   if (message.author.bot) return; // ignore les messages des bots
   const auteurid = message.author.id;
@@ -135,16 +81,4 @@ client.on('messageCreate', (message) => {
 client.on('ready', () => {
   console.log(`Connecté sous ${client.user.tag}!`);
 });
-
-/*client.on('interactionCreate', async interaction => {
-  if (!interaction.isChatInputCommand()) return;
-
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
-  }
-  if (interaction.commandName === 'papillon') {
-    await interaction.reply('de lumière !');
-  }
-});*/
-
 client.login(keys["APIKEY"]);
